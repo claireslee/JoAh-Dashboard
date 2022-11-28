48cb2f5beb3157dd0326e914b3dd8033ccc653a5
Claire <clairebearlee0126@gmail.com>
2022-11-27 21:54:14 -0500

    usage of pypdf2

--- studentData.sql
+++ studentData.sql
@@ -1,5 +1,5 @@
 CREATE TABLE students (id INTEGER PRIMARY KEY, lastname TEXT, firstname TEXT, username TEXT, pass TEXT);
 INSERT INTO students VALUES (1, "Jones", "Bob", "bobjones123", "BobsGroceries78");
 INSERT INTO students VALUES (2, "Grande", "Ariana", "Agags", "AriBae123");
-INSERT INTO students VALUES (3, "Peters", "Evan", "evanpeters", "ahs0000");
+INSERT INTO students VALUES (3, "Peters", "Evan", "evanpeters", "ahs00000");
 INSERT INTO students VALUES (4, "James", "Theo", "tj1214", "Divergent");
