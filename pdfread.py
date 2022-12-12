ff9e2a6de1a02392d822b7b1c5fd8d7675bd8029
Claire <clairebearlee0126@gmail.com>
2022-12-12 01:34:12 -0500

    contact us page + programs page beginning ui/struc

--- pdfread.py
+++ pdfread.py
@@ -6,13 +6,13 @@ def subtract(num1, num2):
 
 # creating a pdf file object
 pdfFileObj = open('Hello world!.pdf', 'rb')
-  
+
 # creating a pdf reader object
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-  
+
 # printing number of pages in pdf file
 print(pdfReader.numPages)
-  
+
 # creating a page object
 pageObj = pdfReader.getPage(0)
   
