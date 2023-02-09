a446c4b836b2c1237f4fd1771268e825c9774f56
Claire <clairebearlee0126@gmail.com>
2023-02-09 11:19:41 -0500

    teacher/admin can add new student on dashboardd

--- mysite/website/models.py
+++ mysite/website/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length = 50)
+class Student(models.Model):
     firstname = models.CharField(max_length = 50)
     lastname = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 50)
     passwd = models.CharField(max_length = 50)
     classname = models.CharField(max_length = 100)
-    email = models.EmailField(max_length=100);
+    email = models.EmailField(max_length=100)
     
     def __str__(self): # what it returns
         return self.firstname + ' ' + self.lastname
